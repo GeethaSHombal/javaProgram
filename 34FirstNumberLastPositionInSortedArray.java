58505d7e64d63feb9e5fb7e0bd85bf922d1b410f
Geetha S Hombal <87638236+GeethaSHombal@users.noreply.github.com>
2022-03-11 12:01:14 +0530

    Finding First Number and Last Position of Element in Sorted Array solution

---
+++ 34FirstNumberLastPositionInSortedArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int Tleft = find(target, nums, 0);
+        if (Tleft == nums.length || nums[Tleft] != target) 
+            return new int[] {-1, -1};
+        return new int[] {Tleft, find(target+1, nums, Tleft) - 1};
+    }
+    public int find(int target, int[] arr, int left) {
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + right >> 1;
+            if (arr[mid] < target) 
+                left = mid + 1;
+            else 
+                right = mid - 1;
+        }
+        return left;
+    }
+}
